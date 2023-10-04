b3fc96b2542baa0f2ad0e38c343392f56caf56c1
Uzair Yousaf <uzairyousaf1@icloud.com>
2023-10-04 12:52:29 +0100

    Completed chapter 1 of Bro Code Java Full Course. Learned escape sequences and the difference between print and println

---
+++ Chapter1_Intro.java
@@ -0,0 +1,14 @@
+public class Chapter1_Intro {
+
+    public static void main(String[] args) {
+
+        System.out.println("I love pizza");
+        System.out.println("Especially stuffed crust!");
+
+        System.out.println("\thello");  //tab before or after print statement
+        System.out.println("hello\n");  //new line after or before print statement
+        System.out.println("\"hello\""); //add quotes to print statement
+
+        //println makes new line after print statement, print doesn't
+    }
+}
