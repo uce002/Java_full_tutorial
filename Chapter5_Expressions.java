9d273c3b7106ffb3173beb2106aba15bb19f65e1
Uzair Yousaf <uzairyousaf1@icloud.com>
2023-10-15 09:37:56 +0100

    Completed chapter 5 of Bro Code Java Full Course. Learned how to use expressions

---
+++ Chapter5_Expressions.java
@@ -0,0 +1,18 @@
+public class Chapter5_Expressions {
+
+    public static void main (String[] args) {
+
+        //expression = operands & operators
+        //operand = values, variables, numbers, quantity
+        //operators = + - / %
+
+        int friends = 10;
+
+        friends++; // increment 1. friends--; to decrement 1
+
+        friends = friends / 3; //to make double. double friends = 10. friends = (double) friends / 3
+
+        System.out.println(friends);
+
+    }
+}
