df38d22685554ddf45448dbde7b8f1ee7726f9d8
Uzair Yousaf <uzairyousaf1@icloud.com>
2023-10-15 21:27:11 +0100

    Completed chapter 9 of Bro Code Java Full Course. Leanred how to use if statements and the inportance of order when using else if

---
+++ Chapter9_if_statements.java
@@ -0,0 +1,20 @@
+public class Chapter9_if_statements {
+
+    public static void main(String[] args) {
+
+        //if statement performs a block of code if it's condition is true
+
+        int age = 75;
+
+        if (age>=75) {
+            System.out.println("ok boomer");
+        }
+        else if(age>=18) {
+            System.out.println("you are an adult");//check an additional condition
+        }
+        else {
+            System.out.println("you are not an adult");
+        }
+
+    }
+}
